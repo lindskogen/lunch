238f720903105930f3f337e26cb0535b5035f2f8
Johan Lindskogen <johan.lindskogen@gmail.com>
2018-10-23 13:41:35 +0200

    Add viewport tag

--- head.js
+++ head.js
@@ -6,5 +6,6 @@ export default () => (
       rel="stylesheet"
       href="https://unpkg.com/tachyons@4/css/tachyons.min.css"
     />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
   </Head>
 );
